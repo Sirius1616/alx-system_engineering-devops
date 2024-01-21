30fd340b1123c640ee3b664af9a18a078b8cc426
Sirius1616 <siriusa1.615@gmail.com>
2024-01-20 16:46:47 -0800

    puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,11 @@
 #A puppet that creates a file with file path
+
+file {'/tmp:
+	ensure => directory,
+}
 file {'/tmp/school':
 	content => "I love Puppet\n",
 	owner => 'www-data',
 	group => 'www-data',
-	mode => '0744'
+	mode => '0744',
 }
