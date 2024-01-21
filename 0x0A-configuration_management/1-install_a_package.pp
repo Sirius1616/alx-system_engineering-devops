af6cb6eabaecfec909d8ed00ea1b3aad25a4e6d8
Sirius1616 <siriusa1.615@gmail.com>
2024-01-21 11:53:17 -0800

    puppet killmenow

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3'
+# Puppet Manifest to Install Flask with pip3
+
+# Ensure the directory exists
+file { '/tmp':
+  ensure => directory,
+}
+
+# Install Flask using pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => File['/tmp'],  # Ensure the directory exists before installing
 }
